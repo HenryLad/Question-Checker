6ed8778912c422e4876c9dec0c1320809982c908
Henry Lad <113623013+HenryLad@users.noreply.github.com>
2023-12-10 12:25:39 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,16 +1,34 @@
-﻿int RightAnswers = 0;
+﻿using System.Diagnostics.Metrics;
+
+int RightAnswers = 0;
 int WrongAnswers = 0;
 
 string[] Datei = File.ReadAllLines(@"C:\Users\henry\Dropbox\Htl_Leonding\2_Klasse\CABS\Cabs_Quiz_C#\Cabs_Fragen.txt");
-
-Console.Write("Enter how many questions you want (1 - 63): ");
+int[] AlreadyUsed = new int[Datei.Length];
+Console.Write($"Enter how many questions you want (1 - {Datei.Length}): ");
 int Anzahl = int.Parse(Console.ReadLine()!);
 
 Random random = new Random();
-
+int Counter = 0;
 for (int i = 0; i < Anzahl; i++)
 {
-   int QuestionNumber = random.Next(0, 63);
+   bool AlreadyUsedBool = false;
+   int QuestionNumber;
+   do
+   {
+      QuestionNumber = random.Next(0, Datei.Length);
+      if(AlreadyUsed.Contains(QuestionNumber))
+      {
+         AlreadyUsedBool = true;
+      }
+      else
+      {
+         AlreadyUsed[Counter] = QuestionNumber;
+         AlreadyUsedBool = false;
+      }
+      Counter++;
+   }while(AlreadyUsedBool || Counter > Datei.Length);
+   
    Console.Write(Datei[QuestionNumber].Split(';')[0] + ":");
    string Answer = Console.ReadLine()!.ToLower();
 
@@ -22,7 +40,15 @@ for (int i = 0; i < Anzahl; i++)
    else
    {
       Console.WriteLine("Wrong. The right answer was " + Datei[QuestionNumber].Split(';')[1]);
-      WrongAnswers++;
+      System.Console.Write("Did you feel like you were right anyways? (y/n)");
+      if(Console.ReadLine()!.ToLower() == "y")
+      {
+         RightAnswers++;
+      }
+      else
+      {
+         WrongAnswers++;
+      }
    }
 }
 
